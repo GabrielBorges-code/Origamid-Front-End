a03d6dba120ee31ac6f5e6d46a7831f4aad2a932
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2023-12-27 15:22:59 -0300

    module 3 finished"

--- React-Completo/my-app/src/modulo3/Exemples3.jsx
+++ React-Completo/my-app/src/modulo3/Exemples3.jsx
@@ -1,9 +1,43 @@
-import CallbackAndMemo from "./CallbackAndMemo";
+import { useEffect } from "react";
+import useFetch from "./useFetch";
+import useLocalStorage from "./useLocalStorage";
 
 const Exemples3 = () => {
+  const [product, setProduct] = useLocalStorage("product", "");
+  const { data, request, loading, error } = useFetch();
+
+  const handleClick = ({ target }) => {
+    setProduct(target.innerText);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const { response, json } = await request(
+        "https://ranekapi.origamid.dev/json/api/produto"
+      );
+
+      console.log(response, json);
+    }
+    fetchData();
+  }, [request]);
+
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>carregando...</p>;
+
   return (
     <>
-      <CallbackAndMemo />
+      <p>
+        Produto escolhido: <b>{product}</b>
+      </p>
+      <button onClick={handleClick}>smartphone</button>
+      <button onClick={handleClick}>notebook</button>
+
+      {data &&
+        data.map((product) => (
+          <div key={product.id}>
+            <h1>{product.nome}</h1>
+          </div>
+        ))}
     </>
   );
 };
---
+++ React-Completo/my-app/src/modulo3/useLocalStorage.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = (key, initial) => {
+  const [state, setState] = useState(() => {
+    const local = window.localStorage.getItem(key);
+
+    return local ? local : initial;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, state);
+  }, [state, key]);
+
+  return [state, setState];
+};
+
+export default useLocalStorage;

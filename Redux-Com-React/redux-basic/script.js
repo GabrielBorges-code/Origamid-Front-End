e6b7f4460f9b59cc927c23d77d204250c00e2c5d
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2024-02-06 20:37:13 -0300

    basic redux actions

--- Redux-Com-React/redux-basic/script.js
+++ Redux-Com-React/redux-basic/script.js
@@ -1,18 +1,34 @@
-const initialState = 10;
+const initialState = 0;
 
-const types = {
-  SUM: "sum",
-  INCREMENT: "increment",
+const actionTypes = {
+  SUM: "SUM",
+  INCREMENT: "INCREMENT",
+  OPEN: "OPEN",
+  CLOSE: "CLOSE",
 };
 
-function reducer(state = initialState, action) {
-  //  console.log(action);
+function increment() {
+  return { type: actionTypes.INCREMENT };
+}
 
+function sum(payload) {
+  return { type: actionTypes.SUM, payload: payload };
+}
+
+function openModal() {
+  return { type: actionTypes.OPEN, payload: true };
+}
+
+function closeModal() {
+  return { type: actionTypes.CLOSE, payload: false };
+}
+
+function counter(state = initialState, action) {
   switch (action.type) {
-    case types.SUM:
+    case actionTypes.SUM:
       return state + action.payload;
 
-    case types.INCREMENT:
+    case actionTypes.INCREMENT:
       return state + 1;
 
     default:
@@ -20,13 +36,54 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = Redux.createStore(reducer);
-console.log(store);
+function modal(state = false, action) {
+  switch (action.type) {
+    case actionTypes.OPEN:
+      return true;
 
+    case actionTypes.CLOSE:
+      return false;
+
+    default:
+      return state;
+  }
+}
+
+const reducer = Redux.combineReducers({ counter, modal });
+
+const store = Redux.createStore(reducer);
 let state = store.getState();
-console.log(state);
-store.dispatch({ type: types.SUM, payload: 1 });
-store.dispatch({ type: types.INCREMENT });
-store.dispatch({ type: types.INCREMENT });
+
+console.log(store);
+console.log("valor inicial", state);
+
+store.dispatch(sum(2));
+store.dispatch(increment());
+
 state = store.getState();
-console.log(state);
+
+const button = document.querySelector("#btn-increment");
+const btnModalOpen = document.querySelector("#btn-modal-open");
+const btnModalClose = document.querySelector("#btn-modal-close");
+
+button.addEventListener("click", () => store.dispatch(increment()));
+btnModalOpen.addEventListener("click", () => store.dispatch(openModal()));
+btnModalClose.addEventListener("click", () => store.dispatch(closeModal()));
+
+function render() {
+  const state = store.getState();
+  const total = document.querySelector("#total");
+  total.innerText = state.counter;
+
+  if (state.modal) {
+    total.style.display = "inline-block";
+  } else {
+    total.style.display = "none";
+  }
+}
+
+render();
+
+const unsubscribe = store.subscribe(render);
+
+console.log("após dispatch", state);

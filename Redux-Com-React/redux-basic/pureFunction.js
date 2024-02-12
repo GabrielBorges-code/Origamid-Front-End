43c24b57e4261a7048d0661ee8f44f352869a33a
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2024-02-12 14:43:21 -0300

    immutability

--- Redux-Com-React/redux-basic/pureFunction.js
+++ Redux-Com-React/redux-basic/pureFunction.js
@@ -1,30 +1,50 @@
-function reducer(state = 0, action) {
-  switch (action.type) {
-    case "MODIFY_WIDTH":
-      return action.payload;
+const array1 = [0, 1, 2, 3, 4];
+const array2 = array1.filter((item) => item > 2);
+array1.pop();
 
-    default:
-      return state;
-  }
+const obj1 = {
+  name: "Gabriel B",
+  age: 25,
+};
+
+const obj2 = immer.produce(obj1, (draft) => {
+  draft.age = 30;
+});
+
+function changeAge(obj, age) {
+  obj.age = age;
+  return obj;
 }
+
+const initialState = {
+  name: "Gabriel B",
+  age: 25,
+};
+
+const actionTypes = {
+  CHANGE_NAME: "changeName",
+  CHANGE_AGE: "changeAge",
+};
+
+const reducer = immer.produce((state, action) => {
+  switch (action.type) {
+    case actionTypes.CHANGE_NAME:
+      state.name = action.payload;
+      break;
+    case actionTypes.CHANGE_AGE:
+      state.age = action.payload;
+      break;
+  }
+}, initialState);
 
 const store = Redux.createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-function render() {
-  const box = document.querySelector("#box");
-  box.style.width = store.getState() + "px";
-}
+store.dispatch({ type: actionTypes.CHANGE_NAME, payload: "Maria" });
+store.dispatch({ type: actionTypes.CHANGE_NAME, payload: "João" });
+store.dispatch({ type: actionTypes.CHANGE_AGE, payload: 123 });
+store.dispatch({ type: actionTypes.CHANGE_NAME, payload: "Pedro" });
+store.dispatch({ type: "TESTE", payload: "Pedro" });
 
-render();
-
-store.subscribe(render);
-
-store.dispatch({ type: "MODIFY_WIDTH", payload: 100 });
-store.dispatch({ type: "MODIFY_WIDTH", payload: 20 });
-store.dispatch({ type: "MODIFY_WIDTH", payload: 30 });
-store.dispatch({ type: "MODIFY_WIDTH", payload: 200 });
-
-console.log(store.getState());

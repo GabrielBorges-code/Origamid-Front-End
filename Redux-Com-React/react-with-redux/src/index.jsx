7bb70c05834fa923f9b39359afba22e73c93d498
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2024-02-17 10:03:20 -0300

    refactor files

--- Redux-Com-React/react-with-redux/src/index.jsx
+++ Redux-Com-React/react-with-redux/src/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
-import store from "./store/configureStore.js";
+// import { Provider } from "react-redux";
+import App from "./challengers01/App";
+// import store from "./challengers01/store/configureStore";
+// import App from "./class/App.jsx";
+// import store from "./class/store/configureStore.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );

d365c5fb58b4c76ed03c37a82cbeffc965fdeafe
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2024-02-17 19:26:47 -0300

    challenger section 4

---
+++ Redux-Com-React/react-with-redux/src/challengers01/store/configureStore.js
@@ -0,0 +1,13 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import login from "./login";
+import photos from "./photos";
+import localStorage from "./middleware/localStorage";
+
+const reducer = combineReducers({ login, photos });
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorage), //.concat(logger),
+});
+
+export default store;
--- Redux-Com-React/react-with-redux/src/index.jsx
+++ Redux-Com-React/react-with-redux/src/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import App from "./challengers01/App";
-// import store from "./challengers01/store/configureStore";
+import store from "./challengers01/store/configureStore";
 // import App from "./class/App.jsx";
 // import store from "./class/store/configureStore.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>
 );
